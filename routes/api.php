bac688fee02c9fa2af1499352f03ecaa6b47ca0f
root <root@virtual.histream.live>
2020-10-08 20:06:20 +0000

    comit 01

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user/{id}', function ($id) {
+    return response()->json(['user' => \App\User::find($id)]);
 });
